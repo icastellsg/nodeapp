6605bbcce847382db86b994acbe7a3eee30a2f52
Iván Castells Galindo <icastellsg01@gmail.com>
2024-06-06 20:25:28 +0200

    lint

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const HelloWordService = require( "./services/hello-world" );
+const express = require("express");
+const HelloWordService = require("./services/hello-world");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/:nameToSalute', (req, res) => {
+app.get("/:nameToSalute", (req, res) => {
   res.send(new HelloWordService().greet(req.params.nameToSalute));
-})
+});
 
-module.exports = app
+module.exports = app;
